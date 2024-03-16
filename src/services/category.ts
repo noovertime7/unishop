b9c6ea6214f0d4cfbe0367d8a7ff0bfa9dc9cd2f
noovertime7 <cc17854330572@163.com>
2024-03-16 20:27:59 +0800

    feat: 商品详情完成

--- src/services/category.ts
+++ src/services/category.ts
@@ -1,13 +1,11 @@
-// src/services/category.ts
-
-import type { CategoryChildItem } from '@/types/category'
+import type { CategoryChildItem, CategoryTopItem } from '@/types/category'
 import { http } from '@/utils/http'
 
 /**
  * 分类列表-小程序
  */
 export const getCategoryTopAPI = () => {
-  return http<CategoryChildItem[]>({
+  return http<CategoryTopItem[]>({
     method: 'GET',
     url: '/category/top',
   })
